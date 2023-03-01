07e35278c7d9f9ba1510c5dd032dfffe7659e5ca
MihaelaDanila100 <79402374+MihaelaDanila100@users.noreply.github.com>
2023-03-01 21:48:34 +0200

    added navbar

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FullLayoutComponent } from './components/full-layout/full-layout.component';
+import { MaterialModule } from './material/material.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    FullLayoutComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    NavbarComponent
   ]
 })
 export class SharedModule { }
