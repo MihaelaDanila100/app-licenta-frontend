507f15f1e652324294dcaaff24ecfbfc7763613f
MihaelaDanila100 <79402374+MihaelaDanila100@users.noreply.github.com>
2023-03-27 22:45:04 +0300

    created lines features

--- frontend/src/app/line-pannel/line-pannel.component.ts
+++ frontend/src/app/line-pannel/line-pannel.component.ts
@@ -8,7 +8,10 @@ import { fabric } from 'fabric';
 })
 export class LinePannelComponent implements OnInit {
 
-  private canvas_container: any;
+  private canvas_container!: fabric.Canvas;
+  private line!: fabric.Line;
+  private mouseDown: boolean = false;
+  private continuous: boolean = false;
 
   constructor() { }
 
@@ -18,6 +21,36 @@ export class LinePannelComponent implements OnInit {
       width: window.innerWidth,
       height: 600
     });
+  }
+
+  public startDrawingLine() {
+    this.canvas_container.on('mouse:down', (object: any) => {
+      this.mouseDown = true;
+      const pointer = this.canvas_container.getPointer(object.e);
+      this.line = new fabric.Line([pointer.x, pointer.y, pointer.x, pointer.y], {
+        stroke: 'red',
+        strokeWidth: 3
+      });
+      this.canvas_container.add(this.line);
+      this.canvas_container.requestRenderAll();
+    });
+    this.canvas_container.on('mouse:move', (object: any) => {
+      if(this.mouseDown || this.continuous) {
+        const pointer = this.canvas_container.getPointer(object.e);
+        this.line.set({
+          x2: pointer.x,
+          y2: pointer.y
+        });
+        this.canvas_container.requestRenderAll();
+      }
+    });
+    this.canvas_container.on('mouse:up', () => {
+      this.mouseDown = false; 
+    });
+  }
+
+  public activateContinuous() {
+    this.continuous = !this.continuous;
   }
 
 }
