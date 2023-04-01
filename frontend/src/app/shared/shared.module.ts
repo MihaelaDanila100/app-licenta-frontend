3da114d1a6a3ffae67ed2bf3fc11d35a39d39525
MihaelaDanila100 <79402374+MihaelaDanila100@users.noreply.github.com>
2023-04-01 14:18:58 +0300

    added secondary navbar

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { PrimengComponentsModule } from './primeng-components/primeng-components.module';
 import { SidenavShapesComponent } from './components/sidenav-shapes/sidenav-shapes.component';
 import { TextComponent } from './components/text/text.component';
+import { SecondaryNavbarComponent } from './components/secondary-navbar/secondary-navbar.component';
 
 
 
@@ -16,7 +17,8 @@ import { TextComponent } from './components/text/text.component';
     FullLayoutComponent,
     SidenavComponent,
     SidenavShapesComponent,
-    TextComponent
+    TextComponent,
+    SecondaryNavbarComponent
   ],
   imports: [
     CommonModule,
