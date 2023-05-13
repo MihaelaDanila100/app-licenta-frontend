35b16f6ac86f2d929b76b66cdfc4ca1be6601565
MihaelaDanila100 <mihaela.danila1@s.unibuc.ro>
2023-05-13 23:27:01 +0300

    created edges canvas

--- frontend/src/app/shared/components/edges-types/edges-types.component.ts
+++ frontend/src/app/shared/components/edges-types/edges-types.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GraphService } from '../../services/graph.service';
+import { DrawingService } from '../../services/drawing.service';
 
 @Component({
   selector: 'app-edges-types',
@@ -8,13 +9,26 @@ import { GraphService } from '../../services/graph.service';
 })
 export class EdgesTypesComponent implements OnInit {
 
-  constructor(private graphService: GraphService) { }
+  constructor(private graphService: GraphService, 
+    private drawingService: DrawingService) { }
+  
+  private edgesCanvas!: fabric.Canvas;
+  private edges: fabric.Object[] = [];
 
   ngOnInit(): void {
+    this.edgesCanvas = this.drawingService.createCanvas('edges_canvas', {
+      preserveObjectStacking: true,
+      width: 210
+    });
+    this.initEdges();
   }
 
   public toggleEdgeMode(): void {
     this.graphService.toggleEdges();
+  }
+
+  private initEdges() {
+    console.log("ahaaaa ", this.edgesCanvas)
   }
 
 }
